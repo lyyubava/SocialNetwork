28b41c23fd2d003b79dd703fad8844b7c76a6b46
UbHTKcQh <lyyubava@gmail.com>
2021-05-31 01:41:43 +0300

    added some endpoints

--- src/api/routes/users_routes.py
+++ src/api/routes/users_routes.py
@@ -3,22 +3,34 @@ from flask import request
 from api.utils.responses import response_with
 from api.utils import responses as resp
 from api.models.models import User, UserSchema
+from api.models.models import Post, PostSchema
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 
-user_routes = Blueprint("user_route", __name__)
+user_routes = Blueprint("user_routes", __name__)
 
 
-@user_routes.route('/<string:username>', methods=['GET'])
-def get_use(username):
-    fetch = User.query.filter_by(username=username).first()
+@user_routes.route('/login', methods=['POST'])
+def authenticate_user():
+    try:
+        data = request.get_json()
+        current_user = User.query.filter_by(username=data['username']).first()
+        print(f'AVA: {current_user.avatar}')
+        if not current_user:
+            return response_with(resp.SERVER_ERROR_404)
+        if current_user.check_password(data['password']):
+            access_token = create_access_token(identity=data['username'])
+            return response_with(resp.SUCCESS_201, value={'message': 'Logged in as {}'.format(current_user.username), "access_token": access_token})
+        else:
+            return response_with(resp.UNAUTHORIZED_401)
 
-    user_schema = UserSchema()
-    user = user_schema.dump(fetch)
-    # user = user_schema.dump(fetch)
-    # print(user)
-    return response_with(resp.SUCCESS_200, value={"user": user})
+    except Exception as e:
+        print(e)
+        return response_with(resp.UNAUTHORIZED_401)
 
 
 @user_routes.route('/', methods=['POST'])
+@jwt_required
 def create_user():
     try:
         data = request.get_json()
@@ -32,3 +44,28 @@ def create_user():
     except Exception as e:
         print(e)
         return response_with(resp.INVALID_INPUT_422)
+
+
+@user_routes.route('/<string:username>', methods=['GET'])
+def get_user(username):
+    fetch = User.query.filter_by(username=username).first()
+    user_schema = UserSchema()
+    user = user_schema.dump(fetch)
+    # user = user_schema.dump(fetch)
+    # print(user)
+    return response_with(resp.SUCCESS_200, value={"user": user})
+
+
+@user_routes.route('/<string:username>/posts', methods=['GET'])
+def display_posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return response_with(resp.INVALID_FIELD_NAME_SENT_422)
+    posts = user.posts
+    post_schema = PostSchema()
+    posts_list = list()
+    for post in posts:
+        dumped_post = post_schema.dump(post)
+        posts_list.append(dumped_post)
+    return response_with(resp.SUCCESS_200, value={"post": posts_list})
+
