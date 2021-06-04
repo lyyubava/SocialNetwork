790c293c3e33630afe9fde322ba33c0d2e67c9a2
UbHTKcQh <lyyubava@gmail.com>
2021-06-04 14:33:01 +0300

    registered blueprints

--- src/main.py
+++ src/main.py
@@ -5,14 +5,19 @@ from flask import Flask, jsonify
 from api.config.config import Configuration
 from api.utils.database import db
 from api.routes.users_routes import user_routes
+from api.routes.users_routes import analytics_route
+from api.routes.users_routes import like_route
 from api.utils.schema import ma
-from flask_jwt_extended import JWTManager
+# from flask_jwt_extended import JWTManager
+from api.utils.jwt import jwt
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 app.register_blueprint(user_routes, url_prefix='/api/users')
+app.register_blueprint(like_route, url_prefix='/api/like_post')
+app.register_blueprint(analytics_route, url_prefix='/api/analytics')
 
 
 @app.after_request
@@ -38,9 +43,9 @@ def not_found(e):
     return response_with(resp.SERVER_ERROR_404)
 
 
-jwt = JWTManager(app)
 db.init_app(app)
 ma.init_app(app)
+jwt.init_app(app)
 migrate = Migrate(app, db)
 migrate.init_app(app, db)
 # with app.app_context():
